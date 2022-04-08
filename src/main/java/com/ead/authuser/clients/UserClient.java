59f379644b561e55e690b8a7c43d467e15729ded
BenjaminFernandes <benjamin.fernandes@equiplano.com.br>
2022-04-07 22:37:43 -0300

    Requisição sincrona de authuser com course

---
+++ src/main/java/com/ead/authuser/clients/UserClient.java
@@ -0,0 +1,49 @@
+package com.ead.authuser.clients;
+
+import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
+import com.ead.authuser.services.UtilsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class UserClient {
+
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private UtilsService utilsService;
+
+    //Utilizando API Composition Pattern
+    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
+        //List<CourseDto> searchResult = null;
+        ResponseEntity<ResponsePageDto<CourseDto>> result = null;
+        String url = this.utilsService.createUrl(userId, pageable);
+
+        log.debug("Request URL: {}", url);
+        log.info("Request URL: {}", url);
+        try{
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType =
+                    new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+
+            result = this.restTemplate.exchange(url, HttpMethod.GET,null, responseType);
+            //searchResult = result.getBody().getContent();
+            log.debug("Response Number of Elements: {}", result.getBody().getTotalElements());
+        }catch (HttpStatusCodeException e){
+            log.error("Error request /courses {} ", e);
+        }
+        log.info("Ending equest /courses userID {}", userId);
+        return result.getBody();
+    }
+}
