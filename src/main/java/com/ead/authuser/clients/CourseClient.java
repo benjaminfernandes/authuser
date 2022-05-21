ea0ee1cf519f3e8a31bd34415fb1ec9634ac4cf9
BenjaminFernandes <benjamin.fernandes@equiplano.com.br>
2022-05-20 22:51:58 -0300

    Adicionando resilience4j

--- src/main/java/com/ead/authuser/clients/CourseClient.java
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,11 +3,14 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dtos.CourseDto;
 import com.ead.authuser.dtos.ResponsePageDto;
 import com.ead.authuser.services.UtilsService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -29,6 +34,8 @@ public class CourseClient {
     private String requestUriCourse;
 
     //Utilizando API Composition Pattern
+    //@Retry(name = "retryInstance", fallbackMethod = "retryfallback")//retentativas de acordo com a config feita no application.yml, caso falhe após as 3 tentativas chama o método no fallbackmethod
+    @CircuitBreaker(name = "circuitbreakerInstance", fallbackMethod = "circuitBreakerfallback") //fallbackMethod rota alterantiva caso erro
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
         //List<CourseDto> searchResult = null;
         ResponseEntity<ResponsePageDto<CourseDto>> result = null;
@@ -46,8 +53,20 @@ public class CourseClient {
         }catch (HttpStatusCodeException e){
             log.error("Error request /courses {} ", e);
         }
-        log.info("Ending equest /courses userID {}", userId);
+        log.info("Ending request /courses userID {}", userId);
         return result.getBody();
     }
 
+    //métodos que retornam uma pagina vazia para não retornar o erro. Ideial seria ter um erro customizado e retornar
+    public Page<CourseDto> circuitBreakerfallback(UUID userId, Pageable pageable, Throwable t){
+        log.error("Inside circuit breaker fallback, cause {}", t.toString());
+        List<CourseDto> searchResult = new ArrayList<>();
+        return new PageImpl<>(searchResult);
+    }
+
+    public Page<CourseDto> retryfallback(UUID userId, Pageable pageable, Throwable t){
+        log.error("Inside retry retryfallback, cause {}", t.toString());
+        List<CourseDto> searchResult = new ArrayList<>();
+        return new PageImpl<>(searchResult);
+    }
 }
