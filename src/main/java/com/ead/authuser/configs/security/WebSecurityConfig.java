4f8884b700eb43745d5f99cd2e93a54b5f7902bb
BenjaminFernandes <benjamin.fernandes@equiplano.com.br>
2022-06-16 15:15:21 -0300

    Adicionando basic auth com database

--- src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
+++ src/main/java/com/ead/authuser/configs/security/WebSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.ead.authuser.configs.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,18 +18,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity //desabilita as configs defaults do security
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private AuthenticationEntryPointImpl authenticationEntryPoint;
+
     private static final String[] AUTH_WHITELIST = {
       "/auth/**"
     };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http
                 .httpBasic()
+                .authenticationEntryPoint(this.authenticationEntryPoint)//o que fazer quando houver uma exceção de autenticação
                 .and()
                 .authorizeRequests()
                 .antMatchers(AUTH_WHITELIST).permitAll()
+                .antMatchers(HttpMethod.GET, "/users/**").hasRole("STUDENT")
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable()
@@ -35,14 +44,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password(passwordEncoder().encode("123456"))
-                .roles("ADMIN");
+        auth.userDetailsService(this.userDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
--- src/main/java/com/ead/authuser/repositories/UserRepository.java
+++ src/main/java/com/ead/authuser/repositories/UserRepository.java
@@ -1,10 +1,12 @@
 package com.ead.authuser.repositories;
 
 import com.ead.authuser.models.UserModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -13,4 +15,6 @@ public interface UserRepository extends JpaRepository<UserModel, UUID>, JpaSpeci
     public boolean existsByUsername(String username);
     public boolean existsByEmail(String email);
 
+    @EntityGraph(attributePaths = "roles", type = EntityGraph.EntityGraphType.FETCH)//realiza o fetch de roles na consulta
+    Optional<UserModel> findByUsername(String username);
 }
