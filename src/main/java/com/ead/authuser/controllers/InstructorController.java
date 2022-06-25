d43e065cd9f9d80f50012eea69279025a6614228
BenjaminFernandes <benjamin.fernandes@equiplano.com.br>
2022-06-25 14:54:04 -0300

    adicionando preAuthorize no InstructorController

--- src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -10,6 +10,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -28,6 +29,7 @@ public class InstructorController {
     @Autowired
     private RoleService roleService;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     @PostMapping("/subscription")
     public ResponseEntity<?> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto) {
         Optional<UserModel> userModelOptional = this.userService.findById(instructorDto.getUserId());
